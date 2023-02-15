4c5f6f7daeb8655c47d4bccf8d5fe76c2831edaf
Enrique Montemayor <enrique.montemayor@icloud.com>
2023-02-15 16:10:24 -0600

    Starting a new project

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z $(command -v python3) ]
+then
+	printf '\033[38;2;200;20;20mError \033[39mPlease install python3 before using'
+else
+	python3 -m venv venv
+	source ./venv/bin/activate
+	pip install -r ./requirements.txt
