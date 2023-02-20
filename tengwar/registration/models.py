741ec66219c1e359148cebb013de48af07700779
Hyde <seokjinyoo0716@gmail.com>
2023-02-20 14:24:08 -0600

    fixed .gitignore, readme and added models

--- tengwar/registration/models.py
+++ tengwar/registration/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = models.IntegerField("student id")
+    grad_year = models.IntegerField("graduation year")
+    hours_remaining = models.IntegerField("hours remaining")
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    
+    event_date = models.DateTimeField("event date")
+    num_registered = models.IntegerField("num registered")
+    
+    def __str__(self):
+        return self.name
